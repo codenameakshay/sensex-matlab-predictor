387413f71305de42ee077f059c42ea6a35f8d335
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-03-08 20:49:27 +0530

    Added support for more BSE stocks and now array gets automatically copied

--- SensexCollector.py
+++ SensexCollector.py
@@ -1,9 +1,17 @@
-import quandl
+import quandl, pyperclip
 quandl.ApiConfig.api_key = "dGS1yZ1R5pr4SyzYni-H"
 quandl.read_key()
 bsesensex = list(quandl.get("BSE/SPBSS5IP", returns="numpy"))
+bse = list(quandl.get("BSE/BOM539397", returns="numpy"))
 openval = []
 for i in range(len(bsesensex)):
     openval.append(bsesensex[i][1])
-print(len(openval))
-print(openval)
+#openval2 = []
+#for i in range(len(bse)):
+#    openval2.append(bse[i][1])
+#print(len(openval2))
+#print(openval2)
+pyperclip.copy(str(openval))
+print('The array is already copied')
+
+
